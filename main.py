818b610d65b9c911538bfc4232d157331b526d74
Pasha_Kovalev <pasha0kovalev@gmail.com>
2021-03-09 22:24:57 +0200

    a page for adding a product has been created

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #настройка позволяющая выполнять какие-либо настройки с БД
+db = SQLAlchemy(app)
 
-app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///shop.db'
-db=SQLAlchemy(app)
 
 class Item(db.Model):
-    id=db.Column(db.Integer, primary_key=True) #primaty_ket=True - поле является первичным ключем и при добавлении будет проставляться в автоматическом режиме
-    title=db.Column(db.String(100), nullable=False) #nullable=False - поле не может быть пустым
-    prise=db.Column(db.Integer, nullable=False)
-    isActive=db.Column(db.Boolean, default=True) #default=True поле по умолчанию будет True
+    id = db.Column(db.Integer,
+                   primary_key=True)  # primaty_ket=True - поле является первичным ключем и при добавлении будет проставляться в автоматическом режиме
+    title = db.Column(db.String(100), nullable=False)  # nullable=False - поле не может быть пустым
+    prise = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)  # default=True поле по умолчанию будет True
     # text = db.Column(db.Text, nullable=False)
 
 
-#главная страница
+# главная страница
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#страница с информацией про магазин
+
+# страница с информацией про магазин
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-if __name__=="__main__":
+
+# страница для добавления товара
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    return render_template('create.html')
+
+
+if __name__ == "__main__":
     app.run(debug=True)
